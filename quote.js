a32d927023ae8ba71e65cb9c29cdb42fe45c8311
YusufArda27 <58040592+YusufArda27@users.noreply.github.com>
2020-07-14 11:35:35 +0300

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,10 @@
+
+var quotes=[
+        "Hayat bir hikaye gibidir, ne kadar uzun olduğu değil ne kadar güzel olduğu önemlidir.",
+        "Adalet olmadan bilgi, ilim değil kurnazlıktır.",
+        "Hayat, bisiklet sürmek gibidir. Dengenizi korumak için, devam etmelisiniz." ,
+        "Gideceği limanı bilmeyene hiçbir rüzgardan hayır gelmez.",
+        "Bir şey yapmanın sırrı, yapabileceğinize inanmaktır. Yeterince güçlü yapabileceğinize inandığınız her şeyi yapabilirsiniz. Herhangi bir şey. İnandığınız sürece.",
+        "Hata yapmıyoruz. Sadece mutlu kazalarımız var.",
+        "Sanırım her birimizin altında yatan gizli bir sanatçı var."
+]
